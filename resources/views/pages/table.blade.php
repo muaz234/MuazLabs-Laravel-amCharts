a5a8bab5ad4ebd551883b0bfde8a42ff5ce4c4c8
muaz234 <ahmedmuaz0152@gmail.com>
2020-08-07 02:28:54 +0800

    ui fix

--- resources/views/pages/table.blade.php
+++ resources/views/pages/table.blade.php
@@ -2,33 +2,39 @@
 <link href="assets/global/plugins/datatables/datatables.min.css" type="text/css" rel="stylesheet" />
 
 @section('content-body')
+{{--<br>--}}
+<h1 class="page-title"> Family Information
+{{--    <small>metronic and bootstrap custom navbars, tabs, pills, accordions and resizable tabs based on bootstrap-tabdrop plugin</small>--}}
+</h1>
 
-    <!-- BEGIN TAB PORTLET-->
-    <div class="portlet light bordered">
-        <div class="portlet-title tabbable-line">
-            <div class="caption">
-                <i class="icon-share font-dark"></i>
-                <span class="caption-subject font-dark bold uppercase">Portlet Tabs</span>
+<div class="row">
+    <div class="col-md-12">
+        <!-- BEGIN TAB PORTLET-->
+        <div class="portlet light bordered">
+            <div class="portlet-title tabbable-line">
+                <div class="caption">
+                    <i class="icon-share font-dark"></i>
+                    <span class="caption-subject font-dark bold uppercase">Family Information Tabs</span>
+                </div>
+                <ul class="nav nav-tabs">
+
+
+                    <li class="active">
+                        <a href="#portlet_tab1" data-toggle="tab">Siblings</a>
+                    </li>
+                    <li>
+                        <a href="#portlet_tab2" data-toggle="tab">Spouse</a>
+                    </li>
+                    <li>
+                        <a href="#portlet_tab3" data-toggle="tab">Children</a>
+                    </li>
+                </ul>
             </div>
-            <ul class="nav nav-tabs">
-
-
-                <li class="active">
-                    <a href="#portlet_tab1" data-toggle="tab"> Tab 1 </a>
-                </li>
-                <li>
-                    <a href="#portlet_tab2" data-toggle="tab"> Tab 2 </a>
-                </li>
-                <li>
-                    <a href="#portlet_tab3" data-toggle="tab"> Tab 3 </a>
-                </li>
-            </ul>
-        </div>
-        <div class="portlet-body">
-            <div class="tab-content">
-                <div class="tab-pane active" id="portlet_tab1">
-                    <table class="table table-bordered table-striped " id="tbl_parents" style="width: 100%;">
-                        <thead>
+            <div class="portlet-body">
+                <div class="tab-content">
+                    <div class="tab-pane active" id="portlet_tab1">
+                        <table class="table table-bordered table-striped " id="tbl_parents" style="width: 100%;">
+                            <thead>
                             <th>#</th>
                             <th>Name</th>
                             <th>Age</th>
@@ -36,39 +42,21 @@
                             <th>Location</th>
                             <th>Status</th>
                             <th>Last Activity</th>
-                        </thead>
-                        <tbody>
+                            </thead>
+                            <tbody>
 
-                        </tbody>
-                    </table>
-                </div>
-                <div class="tab-pane" id="portlet_tab2">
-                    <table class="table table-bordered table-striped " id="tbl_spouse" style="width: 100%;">
-                        <thead>
-                        <th>#</th>
-                        <th>Spouse ID</th>
-                        <th>Name</th>
-                        <th>Age</th>
-                        <th>Birth Date</th>
-                        <th>Marriage Date</th>
-                        <th>Status</th>
-                        <th>Last Activity</th>
-                        </thead>
-                        <tbody>
-
-                        </tbody>
-                    </table>
-                </div>
-                <div class="tab-pane" id="portlet_tab3">
+                            </tbody>
+                        </table>
+                    </div>
                     <div class="tab-pane" id="portlet_tab2">
-                        <table class="table table-bordered table-striped " id="tbl_children" style="width: 100%;">
+                        <table class="table table-bordered table-striped " id="tbl_spouse" style="width: 100%;">
                             <thead>
                             <th>#</th>
-                            <th>Parents ID</th>
                             <th>Spouse ID</th>
                             <th>Name</th>
                             <th>Age</th>
                             <th>Birth Date</th>
+                            <th>Marriage Date</th>
                             <th>Status</th>
                             <th>Last Activity</th>
                             </thead>
@@ -76,11 +64,32 @@
 
                             </tbody>
                         </table>
+                    </div>
+                    <div class="tab-pane" id="portlet_tab3">
+                        <div class="tab-pane" id="portlet_tab2">
+                            <table class="table table-bordered table-striped " id="tbl_children" style="width: 100%;">
+                                <thead>
+                                <th>#</th>
+                                <th>Parents ID</th>
+                                <th>Spouse ID</th>
+                                <th>Name</th>
+                                <th>Age</th>
+                                <th>Birth Date</th>
+                                <th>Status</th>
+                                <th>Last Activity</th>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <!-- END TAB PORTLET-->
+
         </div>
-    </div>
-    <!-- END TAB PORTLET-->
+</div>
 
     @endsection
 <script
