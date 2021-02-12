b5ed1af3cc24b07acc7a9c5dec400eaacefd719d
muaz234 <ahmedmuaz0152@gmail.com>
2021-02-12 17:33:36 +0800

    update footer

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,8 +1,8 @@
 <!-- BEGIN FOOTER -->
 <div class="page-footer">
-    <div class="page-footer-inner"> 2016 &copy; Metronic Theme By
-        <a target="_blank" href="http://keenthemes.com">Keenthemes</a> &nbsp;|&nbsp;
-        <a href="http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes" title="Purchase Metronic just for 27$ and get lifetime updates for free" target="_blank">Purchase Metronic!</a>
+    <div class="page-footer-inner"> {{date('Y')}} &copy; All Right Reserved, Idea and Developed by: 
+        <a target="_blank" href="muaz234.github.io">Muaz Ahmed</a> &nbsp;|&nbsp;
+        Care to Contribute? Send me a pull request <a href="https://github.com/muaz234/MuazLabs-Laravel-amCharts" title="Contribute to this repository" target="_blank">Repository</a>
     </div>
     <div class="scroll-to-top">
         <i class="icon-arrow-up"></i>
